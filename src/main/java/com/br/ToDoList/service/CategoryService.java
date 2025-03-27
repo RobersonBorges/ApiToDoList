754f0b4f34fd8e54a7a14dc411545d9e810827da
Roberson Borges <robersonfborges@gmail.com>
2025-03-27 19:55:36 -0300

    implementada validacao para nao permitir exclusao de categorias ja vinculadas a tasks

--- src/main/java/com/br/ToDoList/service/CategoryService.java
+++ src/main/java/com/br/ToDoList/service/CategoryService.java
@@ -2,17 +2,27 @@ package com.br.ToDoList.service;
 
 import com.br.ToDoList.exceptions.ConfigDataResourceNotFoundException;
 import com.br.ToDoList.models.Category;
+import com.br.ToDoList.models.Task;
 import com.br.ToDoList.repository.CategoryRepository;
+import com.br.ToDoList.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
 
     public ResponseEntity<?> createCategory(Category category) {
         validateCategory(category);
@@ -30,6 +40,7 @@ public class CategoryService {
 
     public ResponseEntity<?> deleteCategory(Long id) {
         Category category = categoryRepository.findById(id).orElseThrow(() -> new ConfigDataResourceNotFoundException("Categoria não encontrada"));
+        validateDeletetionCategory(category);
         categoryRepository.delete(category);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
@@ -40,6 +51,13 @@ public class CategoryService {
         }
         if (categoryRepository.existsByNameCategory(category.getNameCategory())) {
             throw new IllegalArgumentException("Category already exist.");
+        }
+    }
+
+    private void validateDeletetionCategory(Category category) {
+        List<Task> taskList = taskRepository.findByCategoriesCodCategory(category.getCodCategory());
+        if (Boolean.FALSE.equals(taskList.isEmpty())) {
+            throw new IllegalArgumentException("Cannot delete category because it is linked to existing tasks.");
         }
     }
 
